fdb5c53f34dd05c3f93e33c3f6e31079a10cac0d
miguelVar <vargas.miguel32@gmail.com>
2020-10-14 21:23:25 -0500

    se inseta en la tabla user a partir de jso

---
+++ app/services/load_json_service.py
@@ -0,0 +1,19 @@
+import json
+from app.models.model_user import User
+
+def create_user_from_json():
+    my_file = open('C:/Users/migue/PycharmProjects/HolaMundo/app/resource/dataUsers.json')
+    data = json.load(my_file)
+
+    for values in data['data']:
+        id_user = values["id"]
+        name = values["name"]
+        email = values["email"]
+        password = values["password"]
+        is_admin = values["is_admin"]
+        user = User(id=id_user, name=name, email=email, password=password, is_admin=is_admin)
+        user.save()
+        print(user)
+    my_file.close()
+
+
